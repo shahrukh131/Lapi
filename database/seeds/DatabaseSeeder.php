dde1ae82170d7526b7dfc9018da8662b7e90f3c9
Shahrukh-akib <Shahrukhakib@gmail.com>
2020-08-26 11:52:29 +0600

    Seeding Part for product and review

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        factory(App\Model\Product::class, 50)->create();
+        factory(App\Model\Review::class, 300)->create();
+
     }
 }
